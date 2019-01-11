5c4bbaf9f7c928418259d1dd7e98ac23e448376e
Patrick Collins <PatrickCP0@Gmail.com>
2019-01-11 18:30:32 -0500

    misc adjustments

--- src/make_icons.sh
+++ src/make_icons.sh
@@ -4,7 +4,7 @@
 cd "${0%/*}"
 
 OUT="./icons"
-SCALE_PRESET=(6 8 10 12 14 16)
+SCALE_PRESET=(6 10 12 16)
 
 UEFI_ASPECT_RATIO_H=4
 UEFI_ASPECT_RATIO_W=5
@@ -22,15 +22,34 @@ SMALL_ICON_BASE_SIZE=6
 
 rm -rf "$OUT"
 
+function render_bitmap_icons() {
+    render_bitmap_considering_aspect_ratio $@
+    #render_bitmap $@
+}
+
+function optimize_png() {
+    output_file=${1}
+    which optipng &> /dev/null && optipng -o7 --quiet "$output_file"
+}
+
+function render_bitmap_considering_aspect_ratio() {
+    resolution=${1}
+    input_file=${2}
+    output_file=$OUT_DIR/`basename ${input_file} svg`png
+    gravity=${3:-"center"}
+    echo "Creating $output_file"
+	inkscape -h=$resolution -w=$(($RATIO_FOR_SQUARE*$resolution/100)) --export-png="$output_file" $input_file > /dev/null \
+ && convert $output_file -gravity $gravity -background transparent -extent "${resolution}x${resolution}" $output_file \
+ && optimize_png $output_file
+}
+
 function render_bitmap() {
     resolution=${1}
     input_file=${2}
     output_file=$OUT_DIR/`basename ${input_file} svg`png
     echo "Creating $output_file"
-    #unfortunately, rEFInd stretches the icons out to what it thinks are squares; otherwise this could mix aspect ratio mismatches.
-	#inkscape -h=$resolution -w=$(($RATIO_FOR_SQUARE*$resolution/100)) --export-png="$output_file" $input_file > /dev/null \
 	inkscape -h=$resolution --export-png="$output_file" $input_file > /dev/null \
- && which optipng &> /dev/null && optipng -o7 --quiet "$output_file"
+ && optimize_png $output_file
 }
 
 for i in ${SCALE_PRESET[@]}; do
@@ -39,15 +58,19 @@ for i in ${SCALE_PRESET[@]}; do
     SMALL_ICON_SIZE=$(($SMALL_ICON_BASE_SIZE*$SCALE))
     OUT_DIR="$OUT/${BIG_ICON_SIZE}_${SMALL_ICON_SIZE}"
     mkdir -p $OUT_DIR
-    for icon in svg/os_* svg/mouse.svg ; do
-        render_bitmap $BIG_ICON_SIZE ${icon}
+    for icon in svg/os_* ; do
+        render_bitmap_icons $BIG_ICON_SIZE ${icon}
     done
     for icon in svg/arrow_* svg/func_* svg/tool_* ; do
-        render_bitmap $SMALL_ICON_SIZE ${icon}
+        render_bitmap_icons $SMALL_ICON_SIZE ${icon}
     done
-    for icon in svg/vol_* svg/background.svg svg/transparent.svg ; do
-        render_bitmap $(($BIG_ICON_SIZE/4)) ${icon}
+    for icon in svg/vol_* svg/transparent.svg ; do
+        render_bitmap_icons $(($BIG_ICON_SIZE/4)) ${icon}
     done
-    render_bitmap $((9*$BIG_ICON_SIZE/8)) svg/selection_big.svg
-    render_bitmap $((4*$SMALL_ICON_SIZE/3)) svg/selection_small.svg
+    render_bitmap_icons $((9*$BIG_ICON_SIZE/8)) svg/selection_big.svg
+    render_bitmap_icons $((4*$SMALL_ICON_SIZE/3)) svg/selection_small.svg
+    render_bitmap_icons $SMALL_ICON_SIZE svg/mouse.svg northwest
 done
+
+OUT_DIR=$OUT
+render_bitmap $BIG_ICON_SIZE svg/background.svg
--- src/update_colors.sh
+++ src/update_colors.sh
@@ -8,21 +8,45 @@ function set_color() {
     sed -i "s/fill:#....../fill:$color/g" $filename
 }
 
+if [ -f ~/.cache/wal/named-colors.sh ]; then
+    source ~/.cache/wal/named-colors.sh
+else
+    background=212121
+    foreground=f5f5f5
+    cursor=ff0000
+    darkest=212121
+    red=e53935
+    green=43a047
+    yellow=fdd835
+    blue=1e88e5
+    pink=d81b60
+    cyan=00acc1
+    brighter=e0e0e0
+    dark=616161
+    accent=ff0000
+    accent_lighter=ff5a36
+    warning=ff00ff
+    brown=6d4c41
+    darker=424242
+    bright=9e9e9e
+    brightest=f5f5f5
+fi
+
 for icon in svg/os_* svg/boot_*  ; do
-    set_color ${icon} "#ff0000"
+    set_color ${icon} ${red}
 done
 for icon in svg/func_* svg/tool_* ; do
-    set_color ${icon} "#424242"
+    set_color ${icon} ${darker}
 done
 for icon in svg/arrow_* ; do
-    set_color ${icon} "#424242"
+    set_color ${icon} ${darker}
 done
 for icon in svg/vol_* ; do
-    set_color ${icon} "#424242"
+    set_color ${icon} ${darker}
 done
 for icon in svg/selection_* ; do
-    set_color ${icon} "#9e9e9e"
+    set_color ${icon} ${blue}
 done
 
-set_color svg/mouse.svg "#616161"
-set_color svg/background.svg "#e0e0e0"
+set_color svg/mouse.svg ${darkest}
+set_color svg/background.svg ${brighter}
